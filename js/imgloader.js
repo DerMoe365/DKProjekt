fab11d11b211b3640a091520554fc05623c86e33
Arthur Krauter <arthur.krauter@hs-augsburg.de>
2018-09-26 12:47:17 +0200

    Comments and Formatting

--- js/imgloader.js
+++ js/imgloader.js
@@ -1,105 +1,101 @@
 
-	var file = [];
-    var isFileUpdated = false; 
+var file = [];
+var isFileUpdated = false;
 
 
-    async function inner(post_data, gallery, alt, container) {
-        console.log(post_data, gallery, alt, container);
-        
-            if (!isFileUpdated)
-		    {
-			    loadFileNameArray(post_data);
-                console.log("Sleeping");
-                await sleep(1000);
-                console.log("Finished sleeping > check again");
-                return inner(post_data, gallery, alt, container);
-            } else {
-                console.log("Files Array => " + file);
-                if(document.getElementById(container).children.length > 0){
-                        console.log("Has Childs");
-                        console.log(document.getElementById(container).children.length);
-                }else{
-			        file.forEach(function(element)
-				    {
-                        console.log(element);
-                        var div = document.createElement("div");
-                        div.setAttribute("class", "tat-gallery col-sm-6 col-md-3");
-                   
-                        var image = document.createElement("a");
-                        image.setAttribute("href", element);
-                        image.setAttribute("data-toggle", "lightbox");
-                        image.setAttribute("data-gallery", gallery);
-                    
-					    var newImage = document.createElement("IMG");
-					    newImage.setAttribute("src", element);
-                        newImage.setAttribute("class", "img-fluid");
-                        newImage.setAttribute("alt", alt);
+async function inner(post_data, gallery, alt, container) {
+    if (!isFileUpdated) {
+        loadFileNameArray(post_data);
+        console.log("Sleeping");
+        await sleep(1000);
+        console.log("Finished sleeping > check again");
+        return inner(post_data, gallery, alt, container);
+    } else {
+        console.log("Files Array => " + file);
+        if (document.getElementById(container).children.length > 0) {
+            console.log("Has Childs");
+            console.log(document.getElementById(container).children.length);
+        } else {
+            file.forEach(function (element) {
+                console.log(element);
+                var div = document.createElement("div");
+                div.setAttribute("class", "tat-gallery col-sm-6 col-md-3");
 
-                        image.appendChild(newImage);
-                        div.appendChild(image);
-					    document.getElementById(container).appendChild(div);
-                    });
-                    file = [];
-                    isFileUpdated = false;
-                }
-                file = [];
-                isFileUpdated = false;
-            }
-            
-        
-        
-    }
-     
-    function sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
-    }
+                var image = document.createElement("a");
+                image.setAttribute("href", element);
+                image.setAttribute("data-toggle", "lightbox");
+                image.setAttribute("data-gallery", gallery);
 
-    async function fetchJSONResponseData(ajax) {
-        try {
-            var myJSONReturn = await ajax;
-            console.log('async myJSONReturn:', myJSONReturn);
-            return myJSONReturn;
-        } catch (error) {
-            console.log('Error in fetchJSONResponseData:', error);
-        }
-    }
+                var newImage = document.createElement("IMG");
+                newImage.setAttribute("src", element);
+                newImage.setAttribute("class", "img-fluid");
+                newImage.setAttribute("alt", alt);
 
-    function ajaxGetFileNames(post_data) {
-        console.log(post_data);
-        return $.when($.ajax({
-                url: '/php/file.php',
-                method: 'POST',
-                data: post_data,
-                dataType: 'json'
-            }))
-            .then(null, function(jqXHR, textStatus, errorThrown) {
-			    console.log("XHR: ", jqXHR);
-				console.log("Err textStatus: ", textStatus);
-				console.log("Err thrown: ", errorThrown);
-                alert("Error sending request: " + errorThrown);
+                image.appendChild(newImage);
+                div.appendChild(image);
+                document.getElementById(container).appendChild(div);
             });
-    }
-    
-    
-
-	function loadFileNameArray(post_data) {
-        try {
-            fetchJSONResponseData(ajaxGetFileNames(post_data)).then(function(data) {
-                    console.log('loadedFileNames data:', data);
-					isFileUpdated = true;
-                    file = data
-                },
-                function(error) {
-                    console.log('Error in loadFileNameArray:', error);
-					isFileUpdated = true;
-					file = "Error Loading FileNames !";
-                });
-        } catch (error) {
-            console.log('Error in loadPatients:', error);
+            file = [];
+            isFileUpdated = false;
         }
+        file = [];
+        isFileUpdated = false;
     }
 
-    
-    
-	
-   
+
+
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function fetchJSONResponseData(ajax) {
+    try {
+        var myJSONReturn = await ajax;
+        console.log('async myJSONReturn:', myJSONReturn);
+        return myJSONReturn;
+    } catch (error) {
+        console.log('Error in fetchJSONResponseData:', error);
+    }
+}
+
+function ajaxGetFileNames(post_data) {
+    console.log(post_data);
+    return $.when($.ajax({
+        url: '/php/file.php',
+        method: 'POST',
+        data: post_data,
+        dataType: 'json'
+    }))
+        .then(null, function (jqXHR, textStatus, errorThrown) {
+            console.log("XHR: ", jqXHR);
+            console.log("Err textStatus: ", textStatus);
+            console.log("Err thrown: ", errorThrown);
+            alert("Error sending request: " + errorThrown);
+        });
+}
+
+
+
+function loadFileNameArray(post_data) {
+    try {
+        fetchJSONResponseData(ajaxGetFileNames(post_data)).then(function (data) {
+            console.log('loadedFileNames data:', data);
+            isFileUpdated = true;
+            file = data
+        },
+            function (error) {
+                console.log('Error in loadFileNameArray:', error);
+                isFileUpdated = true;
+                file = "Error Loading FileNames !";
+            });
+    } catch (error) {
+        console.log('Error in loadPatients:', error);
+    }
+}
+
+
+
+
+
--- js/script.js
+++ js/script.js
@@ -1,99 +1,98 @@
-  // ---------------------------------------------- //
-  // Minimilize Navbar on Scroll
-  // ---------------------------------------------- //
- var w = screen.width;
- var ow;
+// ---------------------------------------------- //
+// Minimilize Navbar on Scroll
+// ---------------------------------------------- //
+var w = screen.width;
+var ow;
 
- $(document).ready(function() {
-    $(window).on("scroll", function() {
-      if(w>968){
-        ow = 600;
-      }
-      if(580<w&&w<968){
-        ow = 380;
-      }
-      if(w<580){
-        ow = 200;
-      }
-      if ($(window).scrollTop() >= ow) {
-        $(".navbar").addClass("compressed");
-      }
-      else {
-        $(".navbar").removeClass("compressed");
-      }
-    });
+$(document).ready(function () {
+  $(window).on("scroll", function () {
+    if (w > 968) {
+      ow = 600;
+    }
+    if (580 < w && w < 968) {
+      ow = 380;
+    }
+    if (w < 580) {
+      ow = 200;
+    }
+    if ($(window).scrollTop() >= ow) {
+      $(".navbar").addClass("compressed");
+    }
+    else {
+      $(".navbar").removeClass("compressed");
+    }
   });
-  
-
-  // ---------------------------------------------- //
-  // Lightbox
-  // ---------------------------------------------- //
-  
-  $(document).on('click', '[data-toggle="lightbox"]', function(event) {
-    event.preventDefault();
-    $(this).ekkoLightbox();
 });
 
 
-  // ---------------------------------------------- //
-  // Navbar-Toggle-Menu
-  // ---------------------------------------------- //
-  
- $('.navbar-nav>li>a').on('click', function(){
-       $('.navbar-collapse').collapse('hide');
-    });
- 
-  
-  // ---------------------------------------------- //
-  // Navbar-Stick
-  // ---------------------------------------------- //
-  
-  $(document).scroll(function () {
-      if ($(window).scrollTop() >= $('header').offset().top) {
-          $('nav').addClass('sticky');
-      } else {
-          $('nav').removeClass('sticky');
-      }
-  });
-  
-  // ---------------------------------------------- //
-  // Scroll Spy
-  // ---------------------------------------------- //
+// ---------------------------------------------- //
+// Lightbox
+// ---------------------------------------------- //
 
-
-    $('body').scrollspy({
-      target: ".navbar", 
-      offset: 50
-    });   
+$(document).on('click', '[data-toggle="lightbox"]', function (event) {
+  event.preventDefault();
+  $(this).ekkoLightbox();
+});
 
 
 // ---------------------------------------------- //
-  // Smooth Scroll
-  // ---------------------------------------------- //
-  
+// Navbar-Toggle-Menu
+// ---------------------------------------------- //
 
-  $(document).ready(function(){
-    // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
-  
-      // Make sure this.hash has a value before overriding default behavior
-      if (this.hash !== "") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
-  
-        // Store hash
-        var hash = this.hash;
-  
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 1000, function(){
-     
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        });
-      } // End if
-    });
+$('.navbar-nav>li>a').on('click', function () {
+  $('.navbar-collapse').collapse('hide');
+});
+
+
+// ---------------------------------------------- //
+// Navbar-Stick
+// ---------------------------------------------- //
+
+$(document).scroll(function () {
+  if ($(window).scrollTop() >= $('header').offset().top) {
+    $('nav').addClass('sticky');
+  } else {
+    $('nav').removeClass('sticky');
+  }
+});
+
+// ---------------------------------------------- //
+// Scroll Spy
+// ---------------------------------------------- //
+
+
+$('body').scrollspy({
+  target: ".navbar",
+  offset: 50
+});
+
+
+// ---------------------------------------------- //
+// Smooth Scroll
+// ---------------------------------------------- //
+
+
+$(document).ready(function () {
+  // Add smooth scrolling to all links
+  $("a").on('click', function (event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function () {
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
   });
+});
 
